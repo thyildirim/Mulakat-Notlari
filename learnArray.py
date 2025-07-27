70b57f3736fcafc0896b6998d93981fac9ee54b6
thyildirim <taha0yldrm@outlook.com>
2025-07-27 14:56:17 +0300

    arrayy tmm

--- learnArray.py
+++ learnArray.py
@@ -1,6 +1,6 @@
 
 # Python'da list dinamik array olarak kullanılır
-arr = [1, 2, 3, 4, 5]
+arr = [1, 5,3,2,4,7,8,6,9,11,10,33,22]
 
 # Erişim (index ile O(1))
 print(arr[2])  # 3
@@ -21,8 +21,16 @@ for i in arr:
     print(i)
 
 print("------------------------------")
-print(arr[0:4])  # [3, 4, 5] (örnek: 1. indexten 3. indexe kadar)
+print("0. indexten 3. indexe kadar" , arr[0:4])  # [3, 4, 5] (örnek: 1. indexten 3. indexe kadar)
 
 print("------------------------------")
 arr.insert(2, 10)  # 2. indexe 10 ekle
-print(arr)
+print("2. indexe 10 eklenmiş hali" , arr)
+
+print("------------------------------")
+arr.sort()  
+print("Sıralı hali " , arr)
+
+print("------------------------------")
+# Eleman sayısı
+print("Eleman sayısı", len(arr))  # 7
